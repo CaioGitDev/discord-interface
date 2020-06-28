98ce6b239025349978b2e0e5ce4ae4e4724140fe
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2020-06-28 20:18:58 +0100

    fix scroll position on load messages

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Container, Messages , InputWrapper, Input, InputIcon } from './styles'; 
 import ChannelMessage, { Mention } from '../ChannelMessage';
 
 const ChannelData : React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>; 
+  useEffect(() => {
+    const div = messagesRef.current;
+    if(div !== null && div !== undefined)
+    {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef]);
   return (
     <Container>
-      <Messages>
+      <Messages ref={messagesRef}>
         { Array.from(Array(15).keys()).map((n) => (
           <ChannelMessage 
           key={n}
           author="Dismissed Dev"
           date="28/06/2020"
           content = "Lorem ipsum!"
-          
           />
         ))
 
